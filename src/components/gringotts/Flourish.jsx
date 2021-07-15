d7660d9a240b6c9a3f21fa84615d3f1ffb8d8acf
Ashley Sands <ashley.sands01@gmail.com>
2021-07-15 15:35:56 -0500

    added books to flourish component

--- src/components/gringotts/Flourish.jsx
+++ src/components/gringotts/Flourish.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { FLOURISH_URL, headers } from '../services'
 
 function Flourish() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataURL = `${FLOURISH_URL}`
+      const res = await axios.get(dataURL, { headers })
+      console.log(res.data.records)
+      setBooks(res.data.records)
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <div>
-      Flourish
+      {books.map((book, index) => {
+        return (
+          <div key={index}>
+            <p>{book.fields?.name}</p>
+            <div>
+              {book.fields.image.map((images) => {
+                return (
+                  <>
+                    <img src={images.url} alt={book.name} />
+                  </>
+                )
+              })}
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
