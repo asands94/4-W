5e86f9bf863a794c959f334ed35fc2690caedae7
Ashley Sands <ashley.sands01@gmail.com>
2021-07-15 12:15:28 -0500

    got functionality of potions game working

--- src/components/potionsclass/Potions.jsx
+++ src/components/potionsclass/Potions.jsx
@@ -9,50 +9,52 @@ export default function Potions() {
   const [count, setCount] = useState(0)
   const [ingredients, setIngredients] = useState([])
 
-  function potionType() {
-    if (count === 0) {
-      return NeutralPotion
-    } else if (count === 12 || 22) {
-      return GoodPotion
-    } else {
-      return BadPotion
-    }
-  }
-
   useEffect(() => {
     const getIngredients = async () => {
       const searchURL = `${POTION_URL}`
       const res = await axios.get(searchURL, { headers })
       setIngredients(res.data.records)
-      console.log(res.data.records[0].fields)
+      console.log(res.data.records)
 
     }
     getIngredients()
   }, [])
 
 
+  function potionType() {
+    if (count === 0) {
+      return NeutralPotion
+    } else if (count === 5) {
+      return GoodPotion
+    } else {
+      return BadPotion
+    }
+  }
 
-
-
-
+  const handleClick = (value) => {
+    if (value === 5) {
+      setCount(count + 5)
+    }
+  }
 
   return (
     <div>
-      <img src={potionType} alt={potionType} />
+      {count}
+      <img src={potionType()} alt={potionType()} />
       {ingredients.map((ingredient, index) => {
         return (
           <div key={index}>
             {ingredient.fields?.ingredientName}
-            {ingredient.fields.images.map((image) => {
+            {ingredient.fields?.images.map((image) => {
               return (
                 <div key={image.filename}>
-                  <button className="Ingredients"><img src={image.thumbnails.small.url} alt={image.filename} /></button>
+                  <button onClick={() => handleClick(ingredient.fields?.value)} className="Ingredients"><img src={image.thumbnails?.small.url} alt={ingredient.fields?.ingredientName} /></button>
                 </div>
               )
             })}
           </div>
         )
       })}
-    </div>
+    </div >
   )
 }
