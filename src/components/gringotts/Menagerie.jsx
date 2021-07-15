78b2fce84b970906a1a6fb1e00d4508be49bf6c4
Ashley Sands <ashley.sands01@gmail.com>
2021-07-15 14:58:18 -0500

    added animals to the menagerie component

--- src/components/gringotts/Menagerie.jsx
+++ src/components/gringotts/Menagerie.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { MENAGERIE_URL, headers } from '../services'
 
 export default function Menagerie() {
+
+  const [animals, setAnimals] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataURL = `${MENAGERIE_URL}`
+      const res = await axios.get(dataURL, { headers })
+      console.log(res.data.records)
+      setAnimals(res.data.records)
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <div>
-
+      {animals.map((animal, index) => {
+        return (
+          <div key={index}>
+            <h1>{animal.fields?.name}</h1>
+            <div>
+              {animal.fields.image.map((images) => {
+                return (
+                  <>
+                    <img src={images.url} alt={animal.fields?.name} />
+                  </>
+                )
+              })}
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- src/components/services/index.js
+++ src/components/services/index.js
@@ -6,3 +6,4 @@ export const headers = { Authorization: `Bearer ${AIRTABLE_KEY}` };
 export const DIRECTORY_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/Character%20Info`
 export const POTION_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/ingredients`
 export const OWL_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/owl%20post`
+export const MENAGERIE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/magical%20menagerie`
