b7f649ba730cf438fbae1774bbf5abc35c5710ff
Ashley Sands <ashley.sands01@gmail.com>
2021-07-15 08:41:24 -0500

    added some styling to directory component

--- src/components/directory/DirectoryResults.jsx
+++ src/components/directory/DirectoryResults.jsx
@@ -16,16 +16,27 @@ export default function DirectoryResults() {
     getResults()
   }, [character])
 
+  function houseColor() {
+    if (results.fields?.house === "Slytherin") {
+      return "green Specific-Details"
+    } else if (results.fields?.house === "Gryffindor") {
+      return "red Specific-Details"
+    } else if (results.fields?.house === "Ravenclaw") {
+      return "blue Specific-Details"
+    } else {
+      return "yellow Specific-Details"
+    }
+  }
+
 
   return (
     <>
-      <div>
-        <h2>Name: {results.fields?.name}</h2>
+      <div className="Characters-Container">
+        <h2>{results.fields?.name}</h2>
         <img className="Characters" src={results.fields?.image} alt={results.fields?.name} />
-        <h3>Boggart: {results.fields?.boggart}</h3>
-        <h3>House: {results.fields?.house}</h3>
-        <h3>Patronus: {results.fields?.patronus}</h3>
-
+        <h3 className="Character-Details">House</h3><p className={houseColor()}>{results.fields?.house}</p>
+        <h3 className="Character-Details">Patronus</h3> <p className="Specific-Details">{results.fields?.patronus}</p>
+        <h3 className="Character-Details">Boggart</h3> <p className="Specific-Details">{results.fields?.boggart}</p>
       </div>
     </>
   )
