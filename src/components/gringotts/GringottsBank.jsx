231c920acab73e3d5d9e469e45ea4546d4ad76a9
Ashley Sands <ashley.sands01@gmail.com>
2021-07-17 17:04:53 -0500

    styling to gringotts component

--- src/components/gringotts/GringottsBank.jsx
+++ src/components/gringotts/GringottsBank.jsx
@@ -1,84 +1,70 @@
-// import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link } from "react-router-dom"
 // import MoneyDisplay from './MoneyDisplay'
-import "./gringotts.css"
 
-// export default function GringottsBank() {
-
-//   const [balance, setBalance] = useState(0)
-
-
-//   return (
-//     <div>
-//       {/* <MoneyDisplay balance={balance} /> */}
-//       <div className="cards-container">
-//         {/* <Link to="/gringotts-shopping"><button className="gringotts-card">Go Buy School Supplies</button></Link> */}
-//         {/* <div className="gringotts-money-display">
-//           <h1>Your Balance</h1>
-//           <h2>{balance} Galleons</h2>
-//         </div> */}
-//       </div>
-
-//       {/* <button onClick={() => setBalance(balance + 1)}>+</button>
-//       <button onClick={() => setBalance(0)}>-</button> */}
-
-//     </div>
-//     // <Box className="box" color="text.primary" clone>
-//     //   <MoneyDisplay />
-//     // </Box>
-//   )
-// }
-
-import * as React from 'react';
+import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import cardimage from "./cardimage.svg"
+import moneyimage from "./moneyimage.svg"
 
-export default function ImgMediaCard() {
+export default function GringottsBank() {
+
+  const [balance, setBalance] = useState(0)
+
   return (
-    <div className="gringotts-card-container">
-      <Card className="title-box" sx={{ maxWidth: 345, maxHeight: 195 }}>
-
+    <div className="card-container2">
+      <Card className="gringotts-cards" style={{ backgroundColor: "rgb(66, 2, 2)" }} sx={{ width: 345, height: 525 }}>
         <CardContent>
-          <Typography variant="h3" color="antiquewhite">
-            Gringotts Wizarding Bank
+          <Typography variant="h4" color="goldenrod">
+            Your Balance
+          </Typography>
+          <Typography variant="h5" color="goldenrod">
+            {balance} Galleons
+          </Typography>
+          <Typography variant="h2">
+            <Button size="large" variant="contained" style={{ backgroundColor: "goldenrod", color: "rgb(66, 2, 2)", width: 200, height: 55 }} onClick={() => setBalance(balance + 50)}>withdraw 50 galleons</Button>
+            <Button size="large" variant="contained" style={{ backgroundColor: "goldenrod", color: "rgb(66, 2, 2)", width: 200, height: 55 }} onClick={() => setBalance(0)}>Return all money</Button>
           </Typography>
         </CardContent>
-
-      </Card>
-
-      <Card className="title-box" sx={{ maxWidth: 345, maxHeight: 195 }}>
-
-        <CardContent>
-          <Typography variant="h3" color="antiquewhite">
-            Gringotts Wizarding Bank
-          </Typography>
-        </CardContent>
-
-      </Card>
-
-      <Card className="bird" sx={{ maxWidth: 345 }}>
-
         <CardMedia
           component="img"
-          alt="book, owl, wand"
-          height="355"
+          alt="Stacks of gold coins"
+          height="400"
+          image={moneyimage}
+          title="Stacks of gold coins"
+        />
+      </Card>
+
+      <div className="middle-card-container2">
+        <Card className="gringotts-cards" style={{ backgroundColor: "rgb(66, 2, 2)" }} sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <Typography variant="h3" color="orange">
+              Gringotts Wizarding Bank
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+
+
+      <Card className="gringotts-cards" style={{ backgroundColor: "rgb(66, 2, 2)" }} sx={{ width: 345, height: 525 }}>
+        <CardMedia
+          component="img"
+          alt="Owl with wand and book"
+          height="400"
           image={cardimage}
-          title="Contemplative Reptile"
+          title="Owl with wand and book"
         />
         <CardContent>
-          <Typography variant="body1" color="goldenRod">
-            It's time to buy some school supplies! You'll need a pet, some books, and a wand.
-            <Link to="/gringotts-shopping"><Button size="small">Go Buy School Supplies</Button></Link>
+          <Typography color="goldenrod">
+            Now that you have money, it's time to get your school books, a pet, and a wand.
+            <Link to="/gringotts-shopping"><Button size="small">Go shopping for school supplies</Button></Link>
           </Typography>
         </CardContent>
-
       </Card>
     </div>
-
   );
 }
