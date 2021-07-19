603821b6ff39d8b13a328c32bb04e02f662e7651
Ashley Sands <ashley.sands01@gmail.com>
2021-07-19 13:56:22 -0500

    handling errors

--- src/components/diary/NewDiary.jsx
+++ src/components/diary/NewDiary.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from "axios"
 import { Link } from "react-router-dom"
-// import { DIARY_URL, headers } from "../services/index.js"
 import diarymagic from "./diarymagic.png"
 import "./diary.css"
 
