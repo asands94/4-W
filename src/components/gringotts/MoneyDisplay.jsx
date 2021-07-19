a197dc0d32b4c3a804bb1b2438c5cbb3d96b8ee4
Ashley Sands <ashley.sands01@gmail.com>
2021-07-19 06:47:14 -0500

    styling

--- src/components/gringotts/MoneyDisplay.jsx
+++ src/components/gringotts/MoneyDisplay.jsx
@@ -6,7 +6,12 @@ export default function MoneyDisplay(props) {
   const { subBalance } = props
   return (
     <div>
-      <Link to="/gringotts"><button className="gringotts-money-display">{subBalance} Galleons</button></Link>
+      <Link to="/gringotts">
+        <button className="gringotts-money-display"><div className="gringotts-money-display-container">
+          Balance
+        </div>
+          {subBalance} Galleons
+        </button></Link>
     </div >
   )
 }
