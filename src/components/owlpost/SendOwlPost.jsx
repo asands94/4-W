84ce5858537649001ef62cb665a0efa1395f1041
Ashley Sands <ashley.sands01@gmail.com>
2021-07-20 12:50:04 -0500

    added max length to my owl post section, thanks to Chris

--- src/components/owlpost/SendOwlPost.jsx
+++ src/components/owlpost/SendOwlPost.jsx
@@ -30,7 +30,7 @@ export default function SendOwlPost(props) {
       <form className="owl-form" onSubmit={handlePost}>
         <input className="owl-form-value" value={poster} name="name" placeholder="Your Name" onChange={(e) => setPoster(e.target.value)}></input>
         <input className="owl-form-value" value={receiver} name="receiver" placeholder="Recipients Name" onChange={(e) => setReceiver(e.target.value)}></input>
-        <input className="owl-form-value" value={post} name="message" placeholder="Message" onChange={(e) => setPost(e.target.value)}></input>
+        <input className="owl-form-value" value={post} name="message" placeholder="Message" maxLength={100} onChange={(e) => setPost(e.target.value)}></input>
         <button className="owl-form-button">send</button>
       </form>
 
