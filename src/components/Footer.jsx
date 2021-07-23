1bec859845e080dbda94b173ae736099a0a562b4
Ashley Sands <ashley.sands01@gmail.com>
2021-07-22 21:26:06 -0500

    updated github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <a href="https://www.linkedin.com/in/ashley-sands-b64827121/"><img id="linkedin" src="https://i.imgur.com/oxGZLGA.png" alt="linkedin logo" /></a>
-      <a href="https://github.com/asands94/AnimePlus"><img id="github" src="https://i.imgur.com/BbYTbcQ.png" alt="github logo" /></a>
+      <a href="https://github.com/asands94/wizards"><img id="github" src="https://i.imgur.com/BbYTbcQ.png" alt="github logo" /></a>
     </footer>
   )
 }
