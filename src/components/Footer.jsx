16080ce3aeb9098663fa28c2a8f9f9031f29d7aa
Ashley Sands <ashley.sands01@gmail.com>
2021-07-22 21:35:11 -0500

    added name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
+      © 2021 Ashley Sands
       <a href="https://www.linkedin.com/in/ashley-sands-b64827121/"><img id="linkedin" src="https://i.imgur.com/oxGZLGA.png" alt="linkedin logo" /></a>
       <a href="https://github.com/asands94/wizards"><img id="github" src="https://i.imgur.com/BbYTbcQ.png" alt="github logo" /></a>
     </footer>
