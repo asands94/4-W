750ff1aff1de50cf2ae74483f0ffcee68af6f87d
Ashley Sands <ashley.sands01@gmail.com>
2021-10-20 17:23:44 -0500

    made answers lowercase

--- src/components/diary/TomsDiary.jsx
+++ src/components/diary/TomsDiary.jsx
@@ -23,11 +23,11 @@ export default function TomsDiary() {
       <div className="diary-container">
         <p className="animated-text">Hello {user}, my name is Tom Marvolo Riddle.</p>
         <p className="animated-text">{user}, are you in Slytherin?</p>
-        <p className="diary-message">{house}</p>
+        <p className="diary-message">{house.toLowerCase()}</p>
       </div>
       <form className="diary-form" onSubmit={handleSubmit}>
         <input value={house} name="message" onChange={(e) => setHouse(e.target.value)}></input>
-        {house === 'Yes' ? (<Link to="/tom-riddle-diary-slytherin"><button>Yes or No</button></Link>)
+        {house === 'yes' ? (<Link to="/tom-riddle-diary-slytherin"><button>Yes or No</button></Link>)
           : (<Link to="/tom-riddle-diary-other"><button>Yes or No</button></Link>)}
 
       </form>
